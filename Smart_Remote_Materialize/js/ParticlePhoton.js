c8f5b68e85da49b22102d95df4c3f70132d6a4fe
Larry Rentz <lrentzjr@ufl.edu>
2019-04-25 15:30:54 -0400

    UI for smart remote

---
+++ Smart_Remote_Materialize/js/ParticlePhoton.js
@@ -0,0 +1,41 @@
+/*
+Future research:
+-importing a js file in another js file (for encapuslation purposes)
+ */
+var accessToken = "efeeddbd97e56369e66d4c3b12ef7202551d90de";
+var deviceID = "4a0026001051373331333230"
+var light_url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
+var receive_url = "https://api.particle.io/v1/devices/" + deviceID + "/receive";
+var send_url = "https://api.particle.io/v1/devices/" + deviceID + "/send";
+
+//light switch
+function switchOn()
+{
+    $.post(light_url, {params: "on", access_token: accessToken });
+}
+
+function switchOff()
+{
+    $.post(light_url, {params: "off", access_token: accessToken });
+}
+
+//bug in JS, probably $(this).attr('id')
+function buttonSelector(param)
+{
+    //var button = $(this).attr('id');
+    var button = param.id;
+    //test
+    //alert(button);
+    $.post(send_url, {params: button , access_token: accessToken });
+    //window.alert(button);
+}
+
+function setup(param)
+{
+    //var status = this.id;
+        //$(this).attr('id'); //make setup button id "true"
+    var status = param.id;
+    //alert(status);
+    $.post(receive_url, {params: status , access_token: accessToken });
+}
+
